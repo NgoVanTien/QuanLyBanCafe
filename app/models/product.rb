f05e5ec583c1d16462c14996c5925236ff46c906
Ngo Van Tien <ngovantien93@gmail.com>
2017-09-16 17:09:08 +0700

    Show_Error_Messages

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ApplicationRecord
   has_many :orders, through: :product_orders
   has_many :product_orders, dependent: :destroy
   belongs_to :category
+
+  validates :category, presence: true
+  validates :name, presence: true
 end
